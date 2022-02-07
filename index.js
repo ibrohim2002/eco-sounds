3a22d9ca3b52d759fb31b03419be4e236e583ad4
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2022-02-08 01:41:07 +0500

    feat: hovers

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ const a4 = document.getElementById("zarynka")
 const a5 = document.getElementById("javoronok")
 const a6 = document.getElementById("slavka")
 var current=a1
+var current1 = forest
 
 forest.addEventListener('click', () => {
+    current1.classList.remove("active")
+    forest.classList.add("active")
+    current1=forest
     main.style.backgroundImage = "url(/assets/img/forest.jpg)"
     current.pause()
     current=a1
@@ -24,6 +28,9 @@ forest.addEventListener('click', () => {
     play.style.display = "none"
 })
 solovey.addEventListener('click', () => {
+    current1.classList.remove("active")
+    solovey.classList.add("active")
+    current1=solovey
     main.style.backgroundImage = "url(/assets/img/solovey.jpg)"
     a2.play()
     current.pause()
@@ -32,6 +39,9 @@ solovey.addEventListener('click', () => {
     play.style.display = "none"
 })
 drozd.addEventListener('click', () => {
+    current1.classList.remove("active")
+    drozd.classList.add("active")
+    current1=drozd
     main.style.backgroundImage = "url(/assets/img/drozd.jpg)"
     a3.play()
     current.pause()
@@ -40,6 +50,9 @@ drozd.addEventListener('click', () => {
     play.style.display = "none"
 })
 zarynka.addEventListener('click', () => {
+    current1.classList.remove("active")
+    zarynka.classList.add("active")
+    current1=zarynka
     main.style.backgroundImage = "url(/assets/img/zarynka.jpg)"
     a4.play()
     current.pause()
@@ -48,6 +61,9 @@ zarynka.addEventListener('click', () => {
     play.style.display = "none"
 })
 javoronok.addEventListener('click', () => {
+    current1.classList.remove("active")
+    javoronok.classList.add("active")
+    current1=javoronok
     main.style.backgroundImage = "url(/assets/img/javoronok.jpg)"
     a5.play()
     current.pause()
@@ -56,6 +72,9 @@ javoronok.addEventListener('click', () => {
     play.style.display = "none"
 })
 slavka.addEventListener('click', () => {
+    current1.classList.remove("active")
+    slavka.classList.add("active")
+    current1=slavka
     main.style.backgroundImage = "url(/assets/img/slavka.jpg)"
     a6.play()
     current.pause()
